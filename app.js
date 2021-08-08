ac0d7bba38f9999ea1b0b2bb980055d414d10796
timtursunov <timtursgram@gmail.com>
2021-08-08 18:51:42 +0600

    almost done

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ window.addEventListener('scroll', (() => {
 
 //slider function
 let currentSlide = 0;
-let totalSlides = 3 
+let totalSlides = 3
+
 let nextBtn = document.querySelector('#next')
 let prevBtn = document.querySelector('#prev')
 let holder = document.querySelector('.holder')
@@ -41,32 +42,43 @@ let holder2 = document.querySelector('#holderDivTwo')
 let holder3 = document.querySelector('#holderDivThree')
 let holder4 = document.querySelector('#holderDivFour')
 let holder5 = document.querySelector('#holderDivFour')
-
-
-
+// text color
+let textTwo = document.querySelector('#textTwo')
+let textThree = document.querySelector('#textThree')
+let textFour = document.querySelector('#textFour')
+// border div animation div
+let slideImageHeadingTwo = document.querySelector('#slideImageHeadingTwo')
+let slideImageHeadingThree = document.querySelector('#slideImageHeadingThree')
+let slideImageHeadingFour = document.querySelector('#slideImageHeadingFour')
 
 let previousSlide = () => {
     currentSlide = currentSlide - 1;
     if (currentSlide >= 0){
-        holder2.classList.add('holder--prev')
         container2.classList.remove('slide--next')
+        holder2.classList.add('holder--prev')
+        textTwo.classList.remove('slide__text--next')
+        slideImageHeadingTwo.classList.remove('slide__image-heading--next')
         let unit = -currentSlide*1405
         let leftPosition = unit+'px'
         holder.style.left = leftPosition
         if(currentSlide === 1){
-            holder3.classList.add('holder--prev')
+            let unit = -currentSlide*1405
+            let leftPosition = unit+'px'
+            holder.style.left = leftPosition
             container3.classList.remove('slide--next')
-            let unit = -currentSlide*1405
-            let leftPosition = unit+'px'
-            holder.style.left = leftPosition
-        } else if (currentSlide === 2){
-            holder4.classList.add('holder--prev')
-            container4.classList.remove('slide--next')
-            let unit = -currentSlide*1405
-            let leftPosition = unit+'px'
-            holder.style.left = leftPosition
-        } 
+            holder3.classList.add('holder--prev')
+            textThree.classList.remove('slide__text--next')
+            slideImageHeadingThree.classList.remove('slide__image-heading--next')
 
+        } else if (currentSlide === 2){
+            let unit = -currentSlide*1405
+            let leftPosition = unit+'px'
+            holder.style.left = leftPosition
+            container4.classList.remove('slide--next')
+            holder4.classList.add('holder--prev')
+            textFour.classList.remove('slide__text--next')
+            slideImageHeadingFour.classList.remove('slide__image-heading--next')
+        } 
     } else {
         console.log('its done')
     }
@@ -77,6 +89,8 @@ let nextSlide = () => {
         if(currentSlide === 1){
             container2.classList.add('slide--next')
             holder2.classList.remove('holder--prev')
+            textTwo.classList.add('slide__text--next')
+            slideImageHeadingTwo.classList.add('slide__image-heading--next')
             let unit = -currentSlide*1405
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
@@ -84,20 +98,23 @@ let nextSlide = () => {
         }  else if (currentSlide === 2) {
             console.log('two')
             container3.classList.add('slide--next')
-            holder3.classList.remove('holder--prev')
+            textThree.classList.add('slide__text--next')
+            slideImageHeadingThree.classList.add('slide__image-heading--next')
             let unit = -currentSlide*1405
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
         } else if (currentSlide === 3){
             console.log('three')
             container4.classList.add('slide--next')
-            holder4.classList.remove('holder--prev')
+            slideImageHeadingFour.classList.add('slide__image-heading--next')
+            textFour.classList.add('slide__text--next')
             let unit = -currentSlide*1405
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
+            nextBtn.disabled= true
         }
     } else {
-        console.log('its done')
+        console.log('other')
     }
 }
 prevBtn.addEventListener('click', ()=> {
